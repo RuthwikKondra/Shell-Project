b314df3b3ba3b18fa18c2a20bcf48378e9da95a5
Kondra Ruthwik <ruthwikkondra13@gmail.com>
2024-04-24 19:45:11 +0530

    modified

--- backend.sh
+++ backend.sh
@@ -79,7 +79,7 @@ VALIDATE $? "Enabiling backend"
 dnf install mysql -y &>LOGFILE
 VALIDATE $? "installing mysql"
 
-mysql -h 172.31.84.208 -uroot -p${mysql_root_password} < /app/schema/backend.sql &>>$LOGFILE
+mysql -h 172.31.84.208 -u root -p${mysql_root_password} < /app/schema/backend.sql &>>$LOGFILE
 VALIDATE $? "Schema loading"
 
 systemctl restart backend &>LOGFILE
